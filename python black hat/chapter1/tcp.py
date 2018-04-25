5ef3e42ab1a2fea456a144e2a628d647d4626353
engaboda <abodaelkaass@yahoo.com>
2018-04-25 20:48:54 +0200

    Add files via upload

---
+++ python black hat/chapter1/tcp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import socket
+target_host = "www.google.com"
+target_port = 80
+
+#create a socket object
+
+client = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+client.connect((target_host,target_port))
+client.send("GET / HTTP/1.1\r\nHOST: www.google.com\r\n\r\n")
+response = client.recv(4096)
+print response
---
+++ python black hat/chapter1/udp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+client = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+client.sendto("aaaBbbCCC",(target_host,target_port))
+data , addr = client.recvfrom(4096)
+print data
